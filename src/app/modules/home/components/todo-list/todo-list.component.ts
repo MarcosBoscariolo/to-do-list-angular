555c3c353c46ad7d4bde67325cfa1000a8fc02a1
Marcos Boscariolo <marcos.boscariolo@ibm.com>
2022-06-18 18:18:23 -0300

    desenvolvimento das funções

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  taskList: Array<TaskList> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  deleteTask(index: number): void {
+    this.taskList.splice(index, 1);
+  }
+
+  deleteAllTask(): void {
+    const confirm = window.confirm("Deseja deletar todas as suas tarefas?");
+    if (confirm) {
+      this.taskList = [];
+    }
   }
 
 }
